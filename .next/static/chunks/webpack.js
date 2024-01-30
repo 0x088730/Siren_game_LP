16c31e06a84a81fc8686dcee2c12069a1867bbd3
alexander0923 <alexandermitchell923@gmail.com>
2024-01-30 12:03:04 -0800

    add variable day hour usdt

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23cd4ace853508a7"; }
+/******/ 		__webpack_require__.h = function() { return "a27baef1a0cef594"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
