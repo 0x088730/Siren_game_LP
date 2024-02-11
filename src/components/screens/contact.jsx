96e543276c83cd0fa9bb3f031b4dda7a67d0c517
alexander0923 <alexandermitchell923@gmail.com>
2024-02-11 15:22:31 -0800

    update move page

--- src/components/screens/contact.jsx
+++ src/components/screens/contact.jsx
@@ -1,4 +1,8 @@
 export default function Contact({handleImageLoad}) {
+  const goPlay = () => {
+    const newPageURL = 'https://play.cryptoshowdown.io/';
+    window.open(newPageURL, '_blank');
+  }
   return (
     <div className="w-full pt-32 bg-[url('../../public/assets/images/footer/back.jpg')] bg-cover">
       <div className="relative w-[350px] h-[300px] lg:w-[772px] lg:h-[700px] bottom-0 mx-auto">
@@ -9,7 +13,7 @@ export default function Contact({handleImageLoad}) {
             <img src="assets/images/footer/tgBtn1.png" alt="" className="w-1/4 duration-500 -translate-y-8 cursor-pointer z-8 lg:-translate-y-14 hover:brightness-125"/>
         </div>
         <div className="">
-            <img src="assets/images/footer/playBtn1.png" alt="" className="w-1/2 mx-auto duration-500 cursor-pointer z-8 translate-y-44 lg:translate-y-72 hover:brightness-125"/>
+            <img src="assets/images/footer/playBtn1.png" alt="" className="w-1/2 mx-auto duration-500 cursor-pointer z-8 translate-y-44 lg:translate-y-72 hover:brightness-125" onClick={goPlay} />
 
         </div>
         
