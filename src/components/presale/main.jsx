b7741b09d0610b1b20d034fada097fcbf03909b3
alexander0923 <alexandermitchell923@gmail.com>
2024-02-22 10:05:30 -0800

    start transaction 0.09

--- src/components/presale/main.jsx
+++ src/components/presale/main.jsx
@@ -65,7 +65,7 @@ export default function MainPresale({
 
     const onAmountClick = (amount) => {
         setusdtamount(amount);
-        setReceiveAmount(Number(amount * 11.1).toFixed(0))
+        setReceiveAmount(Number.parseInt(amount * 11.1).toFixed(0))
     };
 
     const getEnterCode = (value) => {
